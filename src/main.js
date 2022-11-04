59af711c2c33bd1c76f1463a8a91a2c98ea76fc4
Etaro <gakarinara@gmail.com>
2022-11-05 04:40:03 +0600

    add les-1 for vue

--- src/main.js
+++ src/main.js
@@ -1,25 +1,12 @@
-import { diffDates, diffToHtml } from "./datecalc.js"; // Импортируем 2 функции из файла src/datecalc.js.
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-import { formatError } from "./utils.js"; // Импортируем функцию из файла src/utils.js
+Vue.config.productionTip = false
 
-import { isChecked } from "./switch.js";
-
-import { getStart } from "./timer.js";
-
-export const dateCalcForm = document.getElementById("datecalc");
-const dateCalcResult = document.getElementById("datecalc__result");
-dateCalcForm.addEventListener("submit", handleCalcDates);
-function handleCalcDates(event) {
-  dateCalcResult.innerHTML = "";
-  event.preventDefault();
-  let { firstDate, secondDate } = event.target.elements;
-  (firstDate = firstDate.value), (secondDate = secondDate.value);
-  if (firstDate && secondDate) {
-    const diff = diffDates(firstDate, secondDate); // Вычитаем промежуток между датами.
-
-    dateCalcResult.innerHTML = diffToHtml(diff); // Форматируем промежуток, как html и вставляем в DOM.
-  } else
-    dateCalcResult.innerHTML = formatError(
-      "Для расчета промежутка необходимо заполнить оба поля"
-    ); // Если одно из полей не заполнено (или оба), выводим ошибку
-}
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
